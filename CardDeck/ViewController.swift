787737096f5bd63ab8163f188c72cbd6c6683693
StasyHard <reyngardt.as@yandex.ru>
2020-03-30 18:48:27 +0300

    Add card motion animation

--- CardDeck/ViewController.swift
+++ CardDeck/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet private var cardViews: [CardView]!
     
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    lazy var cardBehavior = CardBehavior(in: animator)
+    
+    
     private var deck = Deck()
     
     override func viewDidLoad() {
@@ -29,11 +33,16 @@ class ViewController: UIViewController {
             
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(flipCard(_:)))
             cardView.addGestureRecognizer(tapGesture)
+            
+            cardBehavior.addItem(cardView)
         }
     }
     
     private var faseUpCardViews: [CardView] {
-        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+        return cardViews.filter {
+            $0.isFaceUp &&
+                !$0.isHidden && $0.transform != CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0) &&
+                $0.alpha == 1}
     }
     
     private var faseUpCardViewsMatch: Bool {
@@ -42,10 +51,14 @@ class ViewController: UIViewController {
             faseUpCardViews[0].suit == faseUpCardViews[1].suit
     }
     
+    var lastChousenCardView: CardView?
+    
     @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
         switch recognizer.state {
         case .ended:
-            if let chousenCardView = recognizer.view as? CardView {
+            if let chousenCardView = recognizer.view as? CardView, faseUpCardViews.count < 2 {
+                lastChousenCardView = chousenCardView
+                cardBehavior.removeItem(chousenCardView)
                 //Анимация переворота карты
                 UIView.transition(with: chousenCardView,
                                   duration: 0.6,
@@ -54,6 +67,7 @@ class ViewController: UIViewController {
                                     chousenCardView.isFaceUp = !chousenCardView.isFaceUp
                 },
                                   completion: { finished in
+                                    let cardsToAnimate = self.faseUpCardViews
                                     //если карты совпали увеличиваем карты, потом уменьшаем и они пропадают
                                     if self.faseUpCardViewsMatch {
                                         UIViewPropertyAnimator.runningPropertyAnimator(
@@ -61,7 +75,7 @@ class ViewController: UIViewController {
                                             delay: 0,
                                             options: [],
                                             animations: {
-                                                self.faseUpCardViews.forEach {
+                                                cardsToAnimate.forEach {
                                                     $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
                                                 }
                                         }) //completion
@@ -71,13 +85,13 @@ class ViewController: UIViewController {
                                                 delay: 0,
                                                 options: [],
                                                 animations: {
-                                                    self.faseUpCardViews.forEach {
+                                                    cardsToAnimate.forEach {
                                                         $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
                                                         $0.alpha = 0
                                                     }
                                             }) //completion
                                             { position in
-                                                self.faseUpCardViews.forEach {
+                                                cardsToAnimate.forEach {
                                                     $0.isHidden = true
                                                     $0.alpha = 1
                                                     $0.transform = .identity
@@ -85,24 +99,41 @@ class ViewController: UIViewController {
                                             }
                                         }
                                     } //если карты две и не совпали, переворачиваем обратно лицом вниз
-                                    else if self.faseUpCardViews.count == 2 {
-                                        self.faseUpCardViews.forEach { cardView in
-                                            UIView.transition(
-                                                with: cardView,
-                                                duration: 0.6,
-                                                options: [.transitionFlipFromLeft],
-                                                animations: {
-                                                    cardView.isFaceUp = false
-                                            })
+                                    else if cardsToAnimate.count == 2 {
+                                        if chousenCardView == self.lastChousenCardView {
+                                            cardsToAnimate.forEach { cardView in
+                                                UIView.transition(
+                                                    with: cardView,
+                                                    duration: 0.6,
+                                                    options: [.transitionFlipFromLeft],
+                                                    animations: {
+                                                        cardView.isFaceUp = false
+                                                }, completion: { finished in
+                                                    self.cardBehavior.addItem(cardView)
+                                                })
+                                            }
+                                        }
+                                    } else {
+                                        if !chousenCardView.isFaceUp {
+                                            self.cardBehavior.addItem(chousenCardView)
                                         }
                                     }
-                                    
                 })
             }
         default: break
         }
     }
-    
-    
+}
+
+extension CGFloat {
+    var arc4random: CGFloat {
+        if self > 0 {
+            return CGFloat.random(in: 0..<self)
+        } else if self < 0 {
+            return -CGFloat.random(in: 0..<abs(self))
+        } else {
+            return 0
+        }
+    }
 }
 
