27790343f019e347b3311ac76be7f7121aa03957
StasyHard <reyngardt.as@yandex.ru>
2020-03-30 12:28:26 +0300

    Add card and deck model

---
+++ CardDeck/Model/Deck.swift
@@ -0,0 +1,41 @@
+//
+//  Deck.swift
+//  CardDeck
+//
+//  Created by Anastasia Reyngardt on 30.03.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.allCases {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int.random(in: 0..<self)
+        } else if self < 0 {
+            return -Int.random(in: 0..<abs(self))
+        } else {
+            return 0
+        }
+    }
+}
